dd6084335157286da8c995495e6b711907c721f0
Preshoth Paramalingam <preshoth@gmail.com>
2015-06-04 15:28:14 -0400

    Implemented Google Photo Uploader, need to test

--- src/com/pm/GooglePhotoImporter.java
+++ src/com/pm/GooglePhotoImporter.java
@@ -5,16 +5,19 @@ import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.gdata.data.PlainTextConstruct;
+import com.google.gdata.data.media.MediaFileSource;
+import com.google.gdata.data.photos.PhotoEntry;
+import com.google.gdata.util.ServiceException;
 import org.apache.commons.io.FileUtils;
 
-import com.google.gdata.client.photos.*;
-import com.google.gdata.data.*;
-import com.google.gdata.data.media.*;
-import com.google.gdata.data.photos.*;
-import com.google.api.client.auth.*;
+import com.google.gdata.client.photos.PicasawebService;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,7 +31,7 @@ public class GooglePhotoImporter {
 
     private TokenResponse tokenResponse_;
 
-    public GooglePhotoImporter(String AccessToken, ArrayList<URL> DbUrl){
+    public GooglePhotoImporter(String AccessToken, ArrayList<URL> DbUrl) throws MalformedURLException, IOException, ServiceException{
 
         File f = new File("tmp");
         ArrayList<File> file = new ArrayList<File>();
@@ -42,6 +45,29 @@ public class GooglePhotoImporter {
                 log.log(Level.SEVERE, exception.getMessage());
             }
         }
+        createTokenResponse(AccessToken);
+
+        //This needs to replaced with a contextual URL
+        URL albumPostUrl = new URL("https://picasaweb.google.com/data/feed/api/user/username/albumid/albumid");
+
+        PicasawebService service = new PicasawebService("DropboxTransferTool");
+        service.setOAuth2Credentials(createCredentialWithAccessTokenOnly(tokenResponse_));
+
+        PhotoEntry myPhoto = new PhotoEntry();
+        myPhoto.setTitle(new PlainTextConstruct(file.get(1).getName()));
+        myPhoto.setClient("DropboxTransferTool");
+
+        try {
+            MediaFileSource myMedia = new MediaFileSource(file.get(1), Files.probeContentType(file.get(1).toPath()));
+            myPhoto.setMediaSource(myMedia);
+        }
+        catch (Exception exception){
+            log.log(Level.SEVERE, exception.getMessage());
+        }
+
+        PhotoEntry returnedPhoto = service.insert(albumPostUrl, myPhoto);
+
+
     }
 
     public void createTokenResponse (String AccessToken){
@@ -49,8 +75,7 @@ public class GooglePhotoImporter {
         tokenResponse_.setAccessToken(AccessToken);
     }
 
-    public static GoogleCredential createCredentialWithAccessTokenOnly(
-            HttpTransport transport, JsonFactory jsonFactory, TokenResponse tokenResponse) {
+    public static GoogleCredential createCredentialWithAccessTokenOnly(TokenResponse tokenResponse) {
         return new GoogleCredential().setFromTokenResponse(tokenResponse);
     }
 
