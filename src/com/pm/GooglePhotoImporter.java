1201a0bb446436fa220403dfef41ec6c981614b2
Preshoth Paramalingam <preshoth@gmail.com>
2015-06-05 03:25:12 -0400

    Google Photos now accepts photos, dropbox links need to be fixed

--- src/com/pm/GooglePhotoImporter.java
+++ src/com/pm/GooglePhotoImporter.java
@@ -1,10 +1,13 @@
 package com.pm;
 
-import com.google.api.client.auth.oauth.AbstractOAuthGetToken;
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.TokenResponse;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.gdata.data.PlainTextConstruct;
 import com.google.gdata.data.media.MediaFileSource;
 import com.google.gdata.data.photos.PhotoEntry;
@@ -17,7 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Files;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,16 +33,20 @@ public class GooglePhotoImporter {
     private static final Logger log = Logger.getLogger( GooglePhotoImporter.class.getName() );
 
     private TokenResponse tokenResponse_;
+    private String userId_;
+    private HttpTransport HTTP_TRANSPORT;
+    private JsonFactory JSON_FACTORY;
 
-    public GooglePhotoImporter(String AccessToken, ArrayList<URL> DbUrl) throws MalformedURLException, IOException, ServiceException{
+    public GooglePhotoImporter(String AccessToken, ArrayList<URL> DbUrl) throws MalformedURLException, IOException, ServiceException, GeneralSecurityException{
 
-        File f = new File("tmp");
         ArrayList<File> file = new ArrayList<File>();
+        int count = 0;
         for (URL url : DbUrl){
             try {
+                File f = new File("test.jpg");
                 FileUtils.copyURLToFile(url, f);
                 file.add(f);
-                f.delete();
+                //f.delete();
             }
             catch(Exception exception) {
                 log.log(Level.SEVERE, exception.getMessage());
@@ -47,36 +54,42 @@ public class GooglePhotoImporter {
         }
         createTokenResponse(AccessToken);
 
-        //This needs to replaced with a contextual URL
-        URL albumPostUrl = new URL("https://picasaweb.google.com/data/feed/api/user/username/albumid/albumid");
-
         PicasawebService service = new PicasawebService("DropboxTransferTool");
-        service.setOAuth2Credentials(createCredentialWithAccessTokenOnly(tokenResponse_));
+
+        HTTP_TRANSPORT =  new NetHttpTransport();
+        JSON_FACTORY = new JacksonFactory();
+        Credential credential = createCredentialWithAccessTokenOnly(HTTP_TRANSPORT, JSON_FACTORY, tokenResponse_);
+        service.setOAuth2Credentials(credential);
 
         PhotoEntry myPhoto = new PhotoEntry();
-        myPhoto.setTitle(new PlainTextConstruct(file.get(1).getName()));
+        myPhoto.setTitle(new PlainTextConstruct(file.get(0).getName()));
         myPhoto.setClient("DropboxTransferTool");
 
         try {
-            MediaFileSource myMedia = new MediaFileSource(file.get(1), Files.probeContentType(file.get(1).toPath()));
+            MediaFileSource myMedia = new MediaFileSource(/*file.get(0)*/ new File("a.jpg"), "image/jpg");
             myPhoto.setMediaSource(myMedia);
         }
         catch (Exception exception){
             log.log(Level.SEVERE, exception.getMessage());
         }
-
+        URL albumPostUrl = new URL("https://picasaweb.google.com/data/feed/api/user/default");
         PhotoEntry returnedPhoto = service.insert(albumPostUrl, myPhoto);
+        credential.refreshToken();
 
 
     }
 
     public void createTokenResponse (String AccessToken){
-        tokenResponse_ = new TokenResponse();
+        tokenResponse_ = new GoogleTokenResponse();
         tokenResponse_.setAccessToken(AccessToken);
+        tokenResponse_.setTokenType("Bearer");
+        tokenResponse_.setScope("https://picasaweb.google.com/data/");
     }
 
-    public static GoogleCredential createCredentialWithAccessTokenOnly(TokenResponse tokenResponse) {
-        return new GoogleCredential().setFromTokenResponse(tokenResponse);
+    public static Credential createCredentialWithAccessTokenOnly(
+            HttpTransport transport, JsonFactory jsonFactory, TokenResponse tokenResponse) {
+        return new Credential(BearerToken.authorizationHeaderAccessMethod()).setFromTokenResponse(
+                tokenResponse);
     }
 
 }
--- src/com/pm/Main.java
+++ src/com/pm/Main.java
@@ -2,15 +2,22 @@
 package com.pm;
 
 import com.dropbox.core.DbxException;
+import com.google.gdata.util.ServiceException;
 
 import java.io.IOException;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, DbxException {
+    public static void main(String[] args) throws IOException, DbxException, ServiceException, GeneralSecurityException {
 
-	    DropboxAuthenticator auth = new DropboxAuthenticator("io6bkt0n9uq2opz","4lgybl1aclmjcq8");
-        DropboxDirectoryList list = new DropboxDirectoryList(auth);
-
+        ArrayList<URL> url = new ArrayList<URL>();
+        URL x = new URL("https://www.dropbox.com/s/7uki3b1sayel74q/Boston%20City%20Flow.jpg");
+        url.add(x);
+	    //DropboxAuthenticator auth = new DropboxAuthenticator("io6bkt0n9uq2opz","4lgybl1aclmjcq8");
+        //DropboxDirectoryList list = new DropboxDirectoryList(auth);
+        GooglePhotoImporter test = new GooglePhotoImporter("TOKEN", url);
     }
 }
